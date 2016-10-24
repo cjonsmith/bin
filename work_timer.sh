c57efdb7585703e592f8ef55b3a48d14f9a63103
Cameron Smith <cameronsmith@email.arizona.edu>
2016-10-24 07:35:11 -0700

    fixed bug of trying to acess generated txt file from the current directory.

--- work_timer.sh
+++ work_timer.sh
@@ -1,14 +1,14 @@
 if [ ! -f $HOME/.ista_timehelper.txt  ]; then
     echo "Hidden file ista_timehelper.txt was not found!"
     echo "Making file now..."
-    echo $PS1 > .ista_timehelper.txt # Old PS1 value
-    echo "$(date +%s)" >> .ista_timehelper.txt # Starting time
+    echo $PS1 > $HOME/.ista_timehelper.txt # Old PS1 value
+    echo "$(date +%s)" >> $HOME/.ista_timehelper.txt # Starting time
     PS1="[ON THE CLOCK]$ "
 else
     echo "Hidden file ista_timehelper.txt found!"
     echo "Deleting file, reseting PS1 shell variable, and calculating time now..."
-    oldPS1="$(head -1 .ista_timehelper.txt)"
-    startTime="$(tail -1 .ista_timehelper.txt)"
+    oldPS1="$(head -1 $HOME/.ista_timehelper.txt)"
+    startTime="$(tail -1 $HOME/.ista_timehelper.txt)"
     endTime="$(date +%s)"
     echo "oldPS1: $oldPS1"
     echo "startTime: $startTime"
