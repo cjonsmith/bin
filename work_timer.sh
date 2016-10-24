d77b7c9f52ada07252b43da010e913d26a7ca08b
Cameron Smith <cameronsmith@email.arizona.edu>
2016-10-23 22:05:09 -0700

    Initial commit

---
+++ work_timer.sh
@@ -0,0 +1,20 @@
+if [ ! -f $HOME/.ista_timehelper.txt  ]; then
+    echo "Hidden file ista_timehelper.txt was not found!"
+    echo "Making file now..."
+    echo $PS1 > .ista_timehelper.txt # Old PS1 value
+    echo "$(date +%s)" >> .ista_timehelper.txt # Starting time
+    PS1="[ON THE CLOCK]$ "
+else
+    echo "Hidden file ista_timehelper.txt found!"
+    echo "Deleting file, reseting PS1 shell variable, and calculating time now..."
+    oldPS1="$(head -1 .ista_timehelper.txt)"
+    startTime="$(tail -1 .ista_timehelper.txt)"
+    endTime="$(date +%s)"
+    echo "oldPS1: $oldPS1"
+    echo "startTime: $startTime"
+    echo "endTime: $endTime"
+    echo "total time: $(( (endTime - startTime) / 60)) minutes."
+    PS1=$oldPS1" "
+    rm $HOME/.ista_timehelper.txt
+fi
+
